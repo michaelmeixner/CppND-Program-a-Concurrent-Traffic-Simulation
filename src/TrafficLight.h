93b1fca7179cf460dd6f9073d5fc4d0b659c6da4
Michael Meixner <michael.meixner@lmco.com>
2022-11-10 11:14:12 -0500

    FP.3 implemented

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -19,9 +19,12 @@ template <class T>
 class MessageQueue
 {
 public:
-
+    void send(T &&);
+    T receive();
 private:
-    
+    std::deque<T> _queue;
+    std::condition_variable _cond;
+    std::mutex _mutex;
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
